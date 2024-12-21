fca4ebb04fe437f75dec9a2049972fd3d7d6d9b5
bageeshdattmishra06 <mishravagishdutt@gmail.com>
2024-12-21 22:28:53 +0530

    Initial commit

---
+++ app3/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def aman(request):
+    return HttpResponse(<h1> <marquee> aman is a innocent boy </marquee> </h1>)
